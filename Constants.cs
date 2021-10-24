212bfda7b61f170739b3bf40f49cf77a3f299704
Matt Bishop <withinfocus@gmail.com>
2021-10-24 15:28:16 -0400

    Add vehicle specs (#45)

--- Constants.cs
+++ Constants.cs
@@ -3,5 +3,10 @@ namespace BishopClassicMotors
     public static class Constants
     {
         public const string VehicleId = "VehicleId";
+        public const string Model = "Model";
+        public const string Engine = "Engine";
+        public const string Transmission = "Transmission";
+        public const string ExteriorColor = "ExteriorColor";
+        public const string InteriorColor = "InteriorColor";
     }
 }
